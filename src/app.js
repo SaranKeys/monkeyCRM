fec39d9a5571c6bff9613efa2bb62dfb42918ab0
SaranKeys <saranjeet@promonkey.tech>
2026-07-14 12:12:05 +0530

    limit increased

--- src/app.js
+++ src/app.js
@@ -20,8 +20,8 @@ const app = express();
 
 app.use(helmet()); 
 app.use(cors()); 
-app.use(express.json({ limit: '10kb' })); 
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 app.use(morgan('dev')); 
 
